603de90ff1eb874d83f3fd06ef24898420cf396e
Constantin Seibold <52665174+ConstantinSeibold@users.noreply.github.com>
2024-05-14 10:49:19 +0200

    Update comments to mask_to_coco.py

--- cxas/io_utils/mask_to_coco.py
+++ cxas/io_utils/mask_to_coco.py
@@ -1,38 +1,53 @@
 import numpy as np
-import json
 from pycocotools import mask
 from copy import deepcopy
 
-# Function to convert binary mask to COCO RLE format
-def binary_mask_to_rle(
-                        binary_mask:np.array,
-                    ):
+def binary_mask_to_rle(binary_mask: np.array) -> dict:
     """
+    Convert a binary mask to COCO Run Length Encoding (RLE) format.
+    
+    Args:
+        binary_mask (np.array): Binary mask array.
+
+    Returns:
+        dict: Dictionary containing the RLE-encoded mask.
     """
     mask_encoded = mask.encode(np.asfortranarray(binary_mask.astype(np.uint8)))
     mask_encoded['counts'] = mask_encoded['counts'].decode('utf-8')
     return mask_encoded
-                      
-def rle_to_binary_mask(rle_mask):
+
+def rle_to_binary_mask(rle_mask: dict) -> np.array:
+    """
+    Convert a RLE-encoded mask to a binary mask array.
+
+    Args:
+        rle_mask (dict): Dictionary containing the RLE-encoded mask.
+
+    Returns:
+        np.array: Binary mask array.
+    """
     rle_mask_copy = deepcopy(rle_mask)
-    # print(rle_mask)
     rle_mask_copy['counts'] = rle_mask_copy['counts'].encode('utf-8')
     binary_mask = mask.decode(rle_mask_copy)
     return binary_mask
-  
-def mask_to_annotation(
-                        mask:np.array, 
-                        base_ann_id:int=1,
-                        img_id:int = 1,
-                        ):
+
+def mask_to_annotation(mask: np.array, base_ann_id: int = 1, img_id: int = 1) -> list:
     """
+    Convert mask array to COCO annotation format.
+
+    Args:
+        mask (np.array): Mask array.
+        base_ann_id (int): Base annotation ID.
+        img_id (int): Image ID.
+
+    Returns:
+        list: List of annotations in COCO format.
     """
     annotations = []
     for i in range(mask.shape[0]):
-        if mask[i].sum()==0:
+        if mask[i].sum() == 0:
             continue
         binary_mask = mask[i]
-        # import pdb;pdb.set_trace()
         mask_encoded = binary_mask_to_rle(binary_mask)
         annotation = {
             'id': base_ann_id + i,
@@ -40,12 +55,20 @@ def mask_to_annotation(
             'category_id': i,  # Assuming a single category
             'segmentation': mask_encoded,
             'area': int(np.sum(binary_mask)),
-            'bbox': toBox(mask_encoded).tolist(),
+            'bbox': to_box(mask_encoded).tolist(),
             'iscrowd': 0  # Set to 1 if the mask represents a crowd region
         }
         annotations.append(annotation)
     return annotations
 
+def to_box(binary_mask: dict) -> list:
+    """
+    Convert binary mask to bounding box format.
 
-def toBox(binary_mask):
+    Args:
+        binary_mask (dict): Binary mask.
+
+    Returns:
+        list: Bounding box coordinates.
+    """
     return mask.toBbox(binary_mask)
